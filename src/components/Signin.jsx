b3970fb4388cb7ea3b0c179a22cfe399f13554de
Ponraj kannan <ponrajacc@gmail.com>
2025-03-08 22:20:12 +0530

    Add files via upload

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,10 +1,47 @@
 import './Signin.css';
+import React, { useState } from "react";
+import axios from 'axios';
 import backgroundImage from './assets/bert-b-rhNff6hB41s-unsplash 1.png';
 import googleIcon from './assets/icons8-google-48.png';
 import hideIcon from './assets/icons8-hide-48.png';
 import divider from './assets/divider.png';
 
 const SignIn = () => {
+    const [user, setUser] = useState({
+        userId: 0, // This would typically be auto-generated on the backend
+        userName: "",
+        userEmail: "",
+        userPassword: "",
+        gender: "",
+        sizePreferences: [],
+        favoriteBrands: [],
+        phone: 0,
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+          ...prevUser,
+          [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Send the user object to the Spring Boot backend using axios
+        axios
+        .post("http://localhost:8080/api/user", user) // Replace with your Spring Boot endpoint
+        .then((response) => {
+        console.log("User data submitted successfully:", response.data);
+        })
+        .catch((error) => {
+        console.error("There was an error submitting the user data:", error);
+        });
+
+        console.log(user);
+    };
+
     return (
         <div className="signin-container">
             <img src={backgroundImage} alt="Background" className="signin-background-image" />
@@ -17,10 +54,18 @@ const SignIn = () => {
                     </button>
                 </div>
                 <img src={divider} alt="Divider Icon" className="divider-icon"/>
-                <div className="signin-form">
+                <form onSubmit={handleSubmit} className="signin-form">
                     <div>
                         <label htmlFor="email" className="input-label">Email Address</label>
-                        <input id="email" type="email" className="input-field" />
+                        <input 
+                            id="email" 
+                            type="email" 
+                            className="input-field"
+                            name="userEmail"
+                            value={user.userEmail}
+                            onChange={handleInputChange}
+                            required
+                        />
                     </div>
 
                     <div>
@@ -30,15 +75,23 @@ const SignIn = () => {
                                 <img src={hideIcon} alt="Hide Icon" className="hide-icon"/>UnHide</button>
                         </div>
 
-                        <input id="password" type="password" className="input-field" />
-                        <a href="#" className="forgot-password-link">Forgot your password?</a>
+                        <input 
+                            id="password" 
+                            type="password" 
+                            className="input-field" 
+                            name="userPassword"
+                            value={user.userPassword}
+                            onChange={handleInputChange}
+                            required
+                        />
+                        <a href="#" className="forgot-password-link" >Forgot your password?</a>
                     </div>
 
                     <button className="signin-button">Sign In</button>
                     <p className="signup-prompt">
                         Don’t have an account? <a href="#" className="signup-link">Sign up</a>
                     </p>
-                </div>
+                </form>
             </div>
         </div>
     );
